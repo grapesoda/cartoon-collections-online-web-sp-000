06cdf395ec3bfc43824b5a75bcfc7958c2f82323
Tres Tarwater <trest03@gmail.com>
2020-12-01 19:24:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
 
-def roll_call_dwarves(dwarves)# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |x,y|
     name = x
     num = y+1
@@ -8,22 +7,19 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet(planeteer_calls)# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |x|
     x.capitalize << "!"
   end
 end
 
-def long_planeteer_calls(calls)# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
   calls.any? do |x|
     x.length > 4
   end
 end
 
-def find_the_cheese(strings)# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   strings.find do |x|
     cheese_types.include?(x)
